b3fbc8b7eed10ed119da397c04f9265563b96532
almope1516daw2 <499745.clot@fje.edu>
2017-02-09 17:39:09 +0100

    Second

---
+++ Practica_M06/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Practica_M06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nFacturaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idProducteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantitatDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.facturadetallBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.productesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.clientsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.facturaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.exportButton = new System.Windows.Forms.Button();
+            this.tabPage4.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facturadetallBindingSource)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productesBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facturaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(727, 253);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(721, 247);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nFacturaDataGridViewTextBoxColumn,
+            this.idProducteDataGridViewTextBoxColumn,
+            this.quantitatDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.facturadetallBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(715, 203);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nFacturaDataGridViewTextBoxColumn
+            // 
+            this.nFacturaDataGridViewTextBoxColumn.DataPropertyName = "n_Factura";
+            this.nFacturaDataGridViewTextBoxColumn.HeaderText = "n_Factura";
+            this.nFacturaDataGridViewTextBoxColumn.Name = "nFacturaDataGridViewTextBoxColumn";
+            // 
+            // idProducteDataGridViewTextBoxColumn
+            // 
+            this.idProducteDataGridViewTextBoxColumn.DataPropertyName = "id_Producte";
+            this.idProducteDataGridViewTextBoxColumn.HeaderText = "id_Producte";
+            this.idProducteDataGridViewTextBoxColumn.Name = "idProducteDataGridViewTextBoxColumn";
+            // 
+            // quantitatDataGridViewTextBoxColumn
+            // 
+            this.quantitatDataGridViewTextBoxColumn.DataPropertyName = "Quantitat";
+            this.quantitatDataGridViewTextBoxColumn.HeaderText = "Quantitat";
+            this.quantitatDataGridViewTextBoxColumn.Name = "quantitatDataGridViewTextBoxColumn";
+            // 
+            // facturadetallBindingSource
+            // 
+            this.facturadetallBindingSource.DataSource = typeof(Practica_M06.factura_detall);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.comboBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnGuardar, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.exportButton, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 212);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(715, 32);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // comboBox
+            // 
+            this.comboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(3, 3);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(232, 21);
+            this.comboBox.TabIndex = 0;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGuardar.Location = new System.Drawing.Point(479, 3);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(233, 26);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(735, 279);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // productesBindingSource
+            // 
+            this.productesBindingSource.DataSource = typeof(Practica_M06.productes);
+            // 
+            // productesBindingSource1
+            // 
+            this.productesBindingSource1.DataSource = typeof(Practica_M06.productes);
+            // 
+            // clientsBindingSource
+            // 
+            this.clientsBindingSource.DataSource = typeof(Practica_M06.clients);
+            // 
+            // facturaBindingSource
+            // 
+            this.facturaBindingSource.DataSource = typeof(Practica_M06.factura);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.exportButton.Location = new System.Drawing.Point(241, 3);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(232, 26);
+            this.exportButton.TabIndex = 2;
+            this.exportButton.Text = "Exportar PDF";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 279);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage4.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facturadetallBindingSource)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.productesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productesBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facturaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource productesBindingSource;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.BindingSource facturaBindingSource;
+        private System.Windows.Forms.BindingSource productesBindingSource1;
+        private System.Windows.Forms.BindingSource clientsBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nFacturaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idProducteDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantitatDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource facturadetallBindingSource;
+        private System.Windows.Forms.Button exportButton;
+    }
+}
+
---
+++ Practica_M06/Form1.cs
@@ -0,0 +1,147 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Reflection;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace Practica_M06
+{
+    
+    public partial class Form1 : Form
+    {
+
+        practicam6Entities entity;
+        string table = "";
+        public Form1()
+        {
+            InitializeComponent();
+            fillCombo(comboBox);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.dataGridView1.Columns.Clear();
+            exportButton.Visible = false;
+
+
+        }
+        private void fillCombo(ComboBox comboBox)
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "PracticaM6";
+            if (dbCon.IsConnect())
+            {
+
+                string query = "SHOW TABLES";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string row = "";
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        comboBox.Items.Add(reader.GetValue(i).ToString());
+
+
+                }
+                reader.Close();
+            }
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("##### " + comboBox.Text);
+            entity = new practicam6Entities();
+            this.dataGridView1.AutoGenerateColumns = true;
+            this.dataGridView1.Columns.Clear();
+            //dataGridView1.DataSource = entity.factura;
+            if (comboBox.Text == "factura")
+            {
+                exportButton.Visible = true;
+                dataGridView1.Columns.RemoveAt(5);
+            } else
+            {
+                exportButton.Visible = false;
+            }
+
+            switch (comboBox.Text)
+            {
+                case "productes":
+                    dataGridView1.DataSource = entity.productes;
+                    break;
+                case "factura":
+                    dataGridView1.DataSource = entity.factura;
+                    break;
+                case "factura_detall":
+                    dataGridView1.DataSource = entity.factura_detall;
+                    break;
+                case "clients":
+                    dataGridView1.DataSource = entity.clients;
+                    break;
+
+
+            }
+            
+            
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            entity.SaveChanges();
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            //Creating iTextSharp Table from the DataTable data
+            PdfPTable pdfTable = new PdfPTable(dataGridView1.ColumnCount);
+            pdfTable.DefaultCell.Padding = 3;
+            pdfTable.WidthPercentage = 30;
+            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            pdfTable.DefaultCell.BorderWidth = 1;
+
+            //Adding Header row
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                cell.BackgroundColor = new BaseColor(240, 240, 240);
+                pdfTable.AddCell(cell);
+            }
+
+            //Adding DataRow
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    pdfTable.AddCell(cell.Value.ToString());
+                }
+
+            }
+
+            //Exporting to PDF
+            string folderPath = "C:\\PDFs\\";
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            using (FileStream stream = new FileStream(folderPath + "DataGridViewExport.pdf", FileMode.Create))
+            {
+                Document pdfDoc = new Document(PageSize.A2, 10f, 10f, 10f, 0f);
+                PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                pdfDoc.Add(pdfTable);
+                pdfDoc.Close();
+                stream.Close();
+            }
+        }
+    }
+}
